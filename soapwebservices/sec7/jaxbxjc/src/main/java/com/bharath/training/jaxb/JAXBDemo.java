a83ab1087d31b64c0d58a98fc1584abec2696c8e
PierreSQS <nomo_messina@web.de>
2024-06-23 16:09:21 +0200

    Sec7_Chap53: Unmarshalling Patient XML-file

--- soapwebservices/sec7/jaxbxjc/src/main/java/com/bharath/training/jaxb/JAXBDemo.java
+++ soapwebservices/sec7/jaxbxjc/src/main/java/com/bharath/training/jaxb/JAXBDemo.java
@@ -5,6 +5,8 @@ import com.bharatthippireddy.patient.Patient;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.StringReader;
 import java.io.StringWriter;
 
 public class JAXBDemo {
@@ -21,6 +23,14 @@ public class JAXBDemo {
         marshaller.marshal(patient, stringWriter);
 
         System.out.println("## The Patient XML-File: "+stringWriter);
+
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+
+        Object unmarshalled = unmarshaller.unmarshal(new StringReader(stringWriter.toString()));
+
+        patient = (Patient)unmarshalled;
+
+        System.out.println("## the name of the unmarshalled Patient: "+patient.getName());
 
     }
 }
