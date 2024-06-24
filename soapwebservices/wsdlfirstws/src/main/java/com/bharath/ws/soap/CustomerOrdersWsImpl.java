ae439742a681d92f52b441e571368983cca8fffc
PierreSQS <nomo_messina@web.de>
2024-06-24 15:08:51 +0200

    Sec9_Chap71-XXX: code optimization

--- soapwebservices/wsdlfirstws/src/main/java/com/bharath/ws/soap/CustomerOrdersWsImpl.java
+++ soapwebservices/wsdlfirstws/src/main/java/com/bharath/ws/soap/CustomerOrdersWsImpl.java
@@ -3,20 +3,10 @@ package com.bharath.ws.soap;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import javax.jws.WebService;
 
 import org.apache.cxf.feature.Features;
-
-import com.bharath.ws.trainings.CreateOrdersRequest;
-import com.bharath.ws.trainings.CreateOrdersResponse;
-import com.bharath.ws.trainings.CustomerOrdersPortType;
-import com.bharath.ws.trainings.GetOrdersRequest;
-import com.bharath.ws.trainings.GetOrdersResponse;
-import com.bharath.ws.trainings.Order;
-import com.bharath.ws.trainings.Product;
 
 @Features(features = "org.apache.cxf.feature.LoggingFeature")
 @WebService(endpointInterface = "com.bharath.ws.trainings.CustomerOrdersPortType")
